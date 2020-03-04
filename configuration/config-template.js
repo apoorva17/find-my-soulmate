3c2d86031a17d60cd4fa29c81ac0c948a0519dac
dvp <weeyou90@gmail.com>
2020-03-04 01:30:16 -0800

    sample config file

---
+++ configuration/config-template.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  facebook_api_key: "FB_APP_ID",
+  facebook_api_secret: "FB_APP_SECRET",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  personality_insights_api_key: "PERSONALITY_INSIGHTS_API_KEY",
+  personality_insights_url: "PERSONALITY_INSIGHTS_URL", 
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
