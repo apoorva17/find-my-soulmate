9677fc5574676d7b7834bb9bd67ac3e4c21aae41
LeslieZhao6 <49551359+LeslieZhao6@users.noreply.github.com>
2020-03-22 13:53:34 +0800

    Update doMatch.js

--- doMatch.js
+++ doMatch.js
@@ -1,16 +1,105 @@
 var fs = require('fs');
 var Q = require('q');
 
-var readFile = Q.denodeify(fs.readFile);
-var writeFile = Q.denodeify(fs.writeFile);
+var getPersonality = Q.denodeify('./getPersonality');
+var personalityToVec = Q.denodeify(getPersonality.personalityToVec);
 
-// extract user profile data
-exports.getPersonalityData = function(user) {
-  var arr = user._json.posts.data
-  if (arr.length > 0){
-    return arr
-  } else {
-    return ""
-  }
-}
+getCloseness = function(v1,v2){
+  var norm1 = 0;
   
+  for (var key in v1){
+    norm1 += v1[key]*v1[key];
+    norm1 = Math.sqrt(norm1);
+  }
+  
+  var norm2 = 0;
+  
+  for (var key in v2){
+    norm2 += v2[key]*v2[key];
+    norm2 = Math.sqrt(norm2);
+  }
+  
+  var dot = 0;
+  
+  for (var key in v1){
+    if (key in v2){
+      dot += v1[key]*v2[key];
+    }
+  }
+    return dot / (norm1*norm2);
+}
+
+getTop = function(dict, n){
+  var foo = dict;
+  
+  var props = Object.keys(foo).map(function(key){
+    return{key:key, value:this[key]};
+  }, foo);
+  props.sort(function(p1,p2) {return p2.value-p1.value;});
+  
+  var topNObj = props.slice(0,n).reduce(function(obj,prop){
+    obj[prop.key] = prop.value;
+    return obj;
+  }, {});
+  
+  return topNObj;
+}
+
+getTopDot = function(d1,d2,n){
+  var d3 = {};
+  for (key in d1){
+    if (key in d2){
+      d3[key] = d1[key]*d2[key];
+    }
+  }
+  return getTop(d3, n);
+}
+
+exports.getClosenessAllUser = function(user,cb){
+    var personalityVec;
+    
+    var UsersList;
+    var closeness = {};
+    var personalities = {};
+  
+    var matches = [];
+    
+    function(personality){
+      personalityVec = personalityToVec(personality);
+      
+      UsersList = JSON.parse(usersText);
+      
+      var promises = [];
+      
+      for (user in UsersList){
+        promises.push(getHandle(user));
+      }
+      
+      return promises;
+    }
+    
+    function(){
+      var i = 0;
+      for (user in UsersList){
+        personalities[user] = personalityToVec(arguments[i]);
+        closeness[user]['p'] = {p: getCloseness(personalityVec, personalities[user])};
+        i++;
+      }
+      
+      for (user in closeness){
+        var el = UsersList[user];
+        el['personality'] = getTop(personalities[user], 3);
+        
+        el['math'] = closeness[user]['p'];
+        
+        matches.push(el);
+      }
+      
+      cb(null, (matches));
+    })
+    .catch(function(err){
+      cb(err)
+    })
+    .done();
+}
+        
