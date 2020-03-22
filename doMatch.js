b7da923b1435bd597be01b644f6888837f93cfca
LeslieZhao6 <49551359+LeslieZhao6@users.noreply.github.com>
2020-03-22 11:35:24 +0800

    Update doMatch.js

--- doMatch.js
+++ doMatch.js
@@ -1,1 +1,16 @@
+var fs = require('fs');
+var Q = require('q');
 
+var readFile = Q.denodeify(fs.readFile);
+var writeFile = Q.denodeify(fs.writeFile);
+
+// extract user profile data
+exports.getPersonalityData = function(user) {
+  var arr = user._json.posts.data
+  if (arr.length > 0){
+    return arr
+  } else {
+    return ""
+  }
+}
+  
