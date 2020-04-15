51f2060a300559324a1692f4d980cda94455fb77
LeslieZhao6 <49551359+LeslieZhao6@users.noreply.github.com>
2020-04-15 23:57:02 +0800

    Update getPersonality.js

--- getPersonality.js
+++ getPersonality.js
@@ -1,168 +1,156 @@
 var fs = require('fs');
-// var Q = require('q');
 var app = require('./app');
+const getPersonality = require('./getPersonality');
+const MongoClient = require('mongodb').MongoClient;
+const mongourl = "mongodb://localhost:27017/";
+const dbname = 'FMSdb';
 
 // extract user profile data
 module.exports = {
-	getUser: function(user,cb){
-		var fName = "./example_personality_outputs"+user+".json";
-		var personalityG;
-		
-		fs.readFile(fName)
-		.then(function(content){
-				cb(null, JSON.parse(content));
-		})
-		.catch(function(err){
-				console.log('personality not catched');
-		})
-		.then(function(text){
-				personalityG = text;
-				return personalityG;
-		})
-		.catch(function(err){cb(err)})
-		.done();
+	getUser: function(user){
+		MongoClient.connect(mongourl, function(err, db){
+				if (err) throw err;
+				var dbo = db.db(dbname);
+				
+				dbo.colleciton.find({"name":"{$user}"})(function(err, content){
+						if (err) throw err;
+						var personality = content;
+				});
+				return personality;
+		});
 	},
 	
 	personalityToVec: function(user){
-		var userData = getPersonality.getUser(user)
+	
+				var raw = getPersonality.getUser(user);
+				var name = raw['name'];
+				var gender = raw['gender'];
+				var age = raw['age'];
+				var genderpref = raw['genderpref'];
+				var ageprefmin = raw['ageprefmin'];
+				var ageprefmax = raw['ageprefmax'];
+				var personality = raw['personality']['children'];
+				var needs = raw['needs'];
+				var values = raw['values'];
+				var behavior = raw['behavior'];
+				var consumption_preferences = raw['consumption_preferences']['consumption_preferences'];
+  
+				var vec = {};
 		
-		var raw = userData;
-		var name = raw['name'];
-		var gender = raw['gender'];
-		var age = raw['age'];
-		var genderpref = raw['genderpref'];
-		var ageprefmin = raw['ageprefmin'];
-		var ageprefmax = raw['ageprefmax'];
-		var personality = raw['personality']['children'];
-		var needs = raw['needs'];
-		var values = raw['values'];
-		var behavior = raw['behavior'];
-		var consumption_preferences = raw['consumption_preferences']['consumption_preferences'];
-  
-		var vec = {};
-		
-		vec['age'] = age;
-		vec['gender'] = gender;
-		vec['genderpref'] = genderpref;
-  		vec['ageprefmin'] = ageprefmin;
-  		vec['ageprefmax'] = ageprefmax;
-  
-		function mergeVec(name, percentage){
-			if (name in vec)
-				vec[name] += percentage;
-			else vec[name] = percentage;
-		}
-  
-		for (x of personality){
-			mergeVec( x['name'], x['percentile']);
-			if ('children' in x){
-				for (y of x['children']){
-					mergeVec(y['name'], y['percentile']);
+				vec['age'] = age;
+				vec['gender'] = gender;
+				vec['genderpref'] = genderpref;
+				vec['ageprefmin'] = ageprefmin;
+				vec['ageprefmax'] = ageprefmax;
+	  
+				function mergeVec(name, percentage){
+					if (name in vec)
+						vec[name] += percentage;
+					else vec[name] = percentage;
 				}
-			}
-		}
-		
-		for (x of needs){
-			mergeVec( x['name'], x['percentile']);
-		}
-		
-		for (x of values){
-			mergeVec( x['name'], x['percentile']);
-		}
-		
-		for (x of behavior){
-			mergeVec(x['name'], x['percentage']);
-		}
-		
-		for (x of consumption_preferences){
-			if ('consumption_preferences' in x){
-				for (y of x['consumption_preferences']){
-					mergeVec(y['name'], y['score'])
+		  
+				for (x of personality){
+					mergeVec( x['name'], x['percentile']);
+					if ('children' in x){
+						for (y of x['children']){
+							mergeVec(y['name'], y['percentile']);
+						}
+					}
 				}
-			}
+				
+				for (x of needs){
+					mergeVec( x['name'], x['percentile']);
+				}
+				
+				for (x of values){
+					mergeVec( x['name'], x['percentile']);
+				}
+				
+				for (x of behavior){
+					mergeVec(x['name'], x['percentage']);
+				}
+				
+				for (x of consumption_preferences){
+					if ('consumption_preferences' in x){
+						for (y of x['consumption_preferences']){
+							mergeVec(y['name'], y['score'])
+						}
+					}
+				}
+				return (vec)
 		}
-		return (vec)
-	}
 };
 
-function getUser(user, cb){
-  var fName = "./example_personality_outputs"+user+".json";
-  var personalityG;
-  
-  fs.readFile(fName)
-  .then(function(content){
-    cb(null, JSON.parse(content));
-  })
-  .catch(function(err){
-    console.log('personality not catched');
-  })
-    .then(function(text){
-    personalityG = text;
-    return personalityG;
-  })
-  .catch(function(err){cb(err)})
-  .done();
+function getUser(user){
+	MongoClient.connect(mongourl, function(err, db){
+			if (err) throw err;
+			var dbo = db.db(dbname);
+			
+			dbo.collection.find({"name":"{$user}"})(function(err, content){
+					if (err) throw err;
+					var personality = content;
+			});
+			return personality;
+	});
 };
     
 function personalityToVec(user){
-  var userData = getUser(user)
-  
-  var raw = userData;
-  var name = raw['name'];
-  var gender = raw['gender'];
-  var age = raw['age'];
-  var genderpref = raw['genderpref'];
-  var ageprefmin = raw['ageprefmin'];
-  var ageprefmax = raw['ageprefmax'];
-  var personality = raw['personality']['children'];
-  var needs = raw['needs'];
-  var values = raw['values'];
-  var behavior = raw['behavior'];
-  var consumption_preferences = raw['consumption_preferences']['consumption_preferences'];
-  
-  var vec = {};
 	
-  vec['age'] = age;
-  vec['gender'] = gender;
-  vec['genderpref'] = genderpref;
-  vec['ageprefmin'] = ageprefmin;
-  vec['ageprefmax'] = ageprefmax;
-	
-  vec['age']
-  function mergeVec(name, percentage){
-    if (name in vec)
-      vec[name] += percentage;
-    else vec[name] = percentage;
-  }
-  
-  for (x of personality){
-    mergeVec( x['name'], x['percentile']);
-    if ('children' in x){
-      for (y of x['children']){
-        mergeVec(y['name'], y['percentile']);
-      }
-    }
-  }
-  
-  for (x of needs){
-    mergeVec( x['name'], x['percentile']);
-  }
-  
-  for (x of values){
-    mergeVec( x['name'], x['percentile']);
-  }
-  
-  for (x of behavior){
-    mergeVec( x['name'], x['percentage']);
-  }
-	
-  for (x of consumption_preferences){
-    if ('consumption_preferences' in x){
-      for (y of x['consumption_preferences']){
-	mergeVec(y['name'], y['score'])
-      }
-    }
-  }
-  
-  return (vec)
-};
+			var raw = getUser(user);
+			var name = raw['name'];
+		    var gender = raw['gender'];
+		    var age = raw['age'];
+		    var genderpref = raw['genderpref'];
+		    var ageprefmin = raw['ageprefmin'];
+		    var ageprefmax = raw['ageprefmax'];
+		    var personality = raw['personality']['children'];
+		    var needs = raw['needs'];
+		    var values = raw['values'];
+		    var behavior = raw['behavior'];
+		    var consumption_preferences = raw['consumption_preferences']['consumption_preferences'];
+		  
+		    var vec = {};
+			
+		    vec['age'] = age;
+		    vec['gender'] = gender;
+		    vec['genderpref'] = genderpref;
+		    vec['ageprefmin'] = ageprefmin;
+		    vec['ageprefmax'] = ageprefmax;
+		    
+		    function mergeVec(name, percentage){
+		    	if (name in vec)
+		    		vec[name] += percentage;
+		    	else vec[name] = percentage;
+		    };
+		    
+		    for (x of personality){
+		    	mergeVec( x['name'], x['percentile']);
+		    	if ('children' in x){
+		    		for (y of x['children']){
+		    			mergeVec(y['name'], y['percentile']);
+		    		}
+		    	}
+		    }
+		    
+		    for (x of needs){
+		    	mergeVec( x['name'], x['percentile']);
+		    }
+		  
+		    for (x of values){
+			    mergeVec( x['name'], x['percentile']);
+		    }
+		  
+		    for (x of behavior){
+			    mergeVec( x['name'], x['percentage']);
+		    }
+			
+		    for (x of consumption_preferences){
+		    	if ('consumption_preferences' in x){
+		    		for (y of x['consumption_preferences']){
+		    			mergeVec(y['name'], y['score'])
+		    		}
+		    	}
+		    }
+		    
+		    return (vec)
+}
