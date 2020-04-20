e788b3a2e62034b34b7901aab8d35b696d5a15eb
LeslieZhao6 <49551359+LeslieZhao6@users.noreply.github.com>
2020-04-20 12:44:30 +0800

    Update getPersonality.js

--- getPersonality.js
+++ getPersonality.js
@@ -8,50 +8,26 @@ const collectionName = 'users';
 
 // extract user profile data
 module.exports = {
-	getUser: function(user){
-		MongoClient.connect(mongourl, function(err, db){
-				if (err) throw err;
-				var dbo = db.db(dbname);
-				
-				dbo.colleciton(collectionName).find({"name":"${user}"}, (err, content) => {
-						if (err) throw err;
-						var personality = content;
-				});
-		});
-		return personality;
-	},
-	
-	personalityToVec: function(user){
-		var raw;
-		
-		MongoClient.connect(mongourl, function(err, db){
-				if (err) throw err;
-				var dbo = db.db(dbname);
-				dbo.collection(collectionName).find({"name":"${user}"}, (err, content) => {
-						if (err) throw err;
-						raw = content;
-						console.log(raw);
-				});
-				
-				var name = raw['name'];
+	personalityToVec: function(raw){
+		var name = raw['name'];
 				// var gender = raw['gender'];
 				// var age = raw['age'];
 				// var genderpref = raw['genderpref'];
 				// var ageprefmin = raw['ageprefmin'];
 				// var ageprefmax = raw['ageprefmax'];
-				var personality = raw["personality"]['children'];
+				var personality = raw["personality"];
 				var needs = raw['needs'];
 				var values = raw['values'];
 				var behavior = raw['behavior'];
-				var consumption_preferences = raw['consumption_preferences']['consumption_preferences'];
+				var consumption_preferences = raw['consumption_preferences'];
   
 				var vec = {};
 		
-				vec['age'] = age;
-				vec['gender'] = gender;
-				vec['genderpref'] = genderpref;
-				vec['ageprefmin'] = ageprefmin;
-				vec['ageprefmax'] = ageprefmax;
+				//vec['age'] = age;
+				//vec['gender'] = gender;
+				//vec['genderpref'] = genderpref;
+				//vec['ageprefmin'] = ageprefmin;
+				//vec['ageprefmax'] = ageprefmax;
 	  
 				function mergeVec(name, percentage){
 					if (name in vec)
@@ -59,28 +35,31 @@ module.exports = {
 					else vec[name] = percentage;
 				}
 		  
-				for (x of personality){
-					mergeVec( x['name'], x['percentile']);
+				for (let x of personality){
+					mergeVec(x["name"],x["percentile"]);
 					if ('children' in x){
-						for (y of x['children']){
+						for (let y of x['children']){
+							var name = 
 							mergeVec(y['name'], y['percentile']);
 						}
 					}
 				}
 				
-				for (x of needs){
-					mergeVec( x['name'], x['percentile']);
+				for (let x of needs){
+					mergeVec(x['name'], x['percentile']);
 				}
 				
-				for (x of values){
-					mergeVec( x['name'], x['percentile']);
+				for (let x of values){
+					mergeVec(x['name'], x['percentile']);
 				}
 				
-				for (x of behavior){
-					mergeVec(x['name'], x['percentage']);
+				if (typeof(behavior) != "undefined"){
+					for (let x of behavior){
+						mergeVec(x['name'], x['percentage']);
+					}
 				}
 				
-				for (x of consumption_preferences){
+				for (let x of consumption_preferences){
 					if ('consumption_preferences' in x){
 						for (y of x['consumption_preferences']){
 							mergeVec(y['name'], y['score']);
@@ -88,22 +67,9 @@ module.exports = {
 					}
 				}
 				return vec;
-		});
-	}
+		}
+	
 
-};
-
-function getUser(user){
-	MongoClient.connect(mongourl, function(err, db){
-			if (err) throw err;
-			var dbo = db.db(dbname);
-			
-			dbo.collection(collectionName).find({"name":"${user}"}, (err, content) => {
-					if (err) throw err;
-					var personality = content;
-			});
-	});
-	return personality;
 };
     
 function personalityToVec(user){
@@ -133,11 +99,11 @@ function personalityToVec(user){
 		  
 		    var vec = {};
 	
-		    vec['age'] = age;
-		    vec['gender'] = gender;
-		    vec['genderpref'] = genderpref;
-		    vec['ageprefmin'] = ageprefmin;
-		    vec['ageprefmax'] = ageprefmax;
+		    //vec['age'] = age;
+		    //vec['gender'] = gender;
+		    //vec['genderpref'] = genderpref;
+		    //vec['ageprefmin'] = ageprefmin;
+		    //vec['ageprefmax'] = ageprefmax;
 		    
 		    function mergeVec(name, percentage){
 		    	if (name in vec)
